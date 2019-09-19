5a14e830fbb0a32ed75493718eb712b0ed87e942
ch832648 <54643712+ch832648@users.noreply.github.com>
2019-09-19 13:15:07 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -19,15 +19,27 @@ tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-node = request.XenVM("compute-node")
-node.cores = 4
-node.ram = 4096
-    
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routable_control_ip = "true"
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
 
-node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
+for i in range(3):
+  if i == 0:
+    node = request.XenVM("head")
+    node.routable_control_ip = "true"
+  else:
+    node = request.XenVM("compute-" + str(i))
+    node.cores = 4
+    node.ram = 4096
+    
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
   
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
